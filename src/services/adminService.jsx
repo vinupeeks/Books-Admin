9ac2b5d5c3c12be8f92e8c753cd3c5a49f578bdb
vinupeeks <vinupeeks@gmail.com>
2024-12-13 10:27:34 +0530

    profile api change

--- src/services/adminService.jsx
+++ src/services/adminService.jsx
@@ -1,11 +1,12 @@
 import apiRequest from '../apis/api-request.jsx';
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { getAuthToken } from '../utils/TokenHelper.jsx';
-import RouteConstants from '../constant/Routeconstant.jsx'; 
+import { getAuthToken, getDecodedTokenId } from '../utils/TokenHelper.jsx';
+import RouteConstants from '../constant/Routeconstant.jsx';
 
 // import { userStore } from '../store/userStore';
 // const accessToken = userStore.getState().user.token;
 
+const TokenId = getDecodedTokenId();
 const Token = getAuthToken();
 
 const adminService = {
@@ -15,6 +16,16 @@ const adminService = {
             method: "POST",
             url: `${RouteConstants.LOGIN}`,
             data: admin,
+        });
+    },
+
+    adminProfile: async () => {
+        return apiRequest({
+            method: "GET",
+            url: `/admin/${TokenId}`,
+            headers: {
+                Authorization: "Bearer " + Token
+            }
         });
     },
 
--- src/utils/TokenHelper.jsx
+++ src/utils/TokenHelper.jsx
@@ -1,5 +1,20 @@
+import { jwtDecode } from "jwt-decode";
 
 
 export const getAuthToken = () => {
     return localStorage.getItem('BooksAdminToken');
+};
+
+export const getDecodedTokenId = () => {
+    const Token = localStorage.getItem('BooksAdminToken');
+
+    if (Token) {
+        try {
+            const decodedToken = jwtDecode(Token);
+            let ID = null;
+            return ID = decodedToken.id;
+        } catch (error) {
+            console.error('Error decoding token', error);
+        }
+    }
 };
