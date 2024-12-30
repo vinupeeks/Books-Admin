c8db501cd89084b16d683d419f249b5dcb528293
vinupeeks <vinupeeks@gmail.com>
2024-12-30 17:11:44 +0530

    added some more bug fixes codes

--- src/components/MembersList/MembersList.jsx
+++ src/components/MembersList/MembersList.jsx
@@ -394,9 +394,10 @@ const MembersList = ({ searchTerm, setSearchTerm, membershipType, setMembershipT
       }
       {memberships?.length > 0 ? (
         <div className="overflow-x-auto bg-white shadow-lg rounded-lg">
-          <div className="mb-4 text-black dark:text-gray-300">
-            {currentPage * pageSize + 1} To {currentPage * pageSize + memberships.length} out of {totalCount} List.
-          </div>
+        <div className="mb-4 text-black dark:text-gray-300">
+          {currentPage * pageSize + 1} To {currentPage * pageSize + memberships.length} out of {totalCount} books.
+  
+        </div>
           <table className="min-w-full table-auto">
             <thead className="bg-gray-300">
               <tr>
